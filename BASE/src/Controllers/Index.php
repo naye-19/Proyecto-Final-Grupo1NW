5a16b0a378abe54ac23b796fb0f350b2c890a0bb
Carlos Ardon <ardon7528@gmail.com>
2022-07-17 15:01:31 -0600

    Creacion del index

--- BASE/src/Controllers/Index.php
+++ BASE/src/Controllers/Index.php
@@ -10,7 +10,6 @@
  * @link     http://
  */
 namespace Controllers;
-
 /**
  * Index Controller
  *
@@ -27,10 +26,35 @@ class Index extends PublicController
      *
      * @return void
      */
+
     public function run() :void
     {
-        $viewData = array();
-        \Views\Renderer::render("index", $viewData);
+        $dataview["items"] = \Dao\Client\Productos::getProductosRecientes();
+
+        $max_description_length = 50;
+        
+        foreach($dataview["items"] as $key => $value)
+        {
+            if(strlen($dataview["items"][$key]["ProdDescripcion"]) > $max_description_length)
+            {
+                $string = $value["ProdDescripcion"];
+                $offset = ($max_description_length - 3) - strlen($string);
+                $dataview["items"][$key]["ProdDescripcion"] = substr($string, 0, strrpos($string, ' ', $offset)) . '...';
+            }
+
+            $precioFinal = ($value["ProdPrecioVenta"]) + ($value["ProdPrecioVenta"] * 0.15); 
+            $dataview["items"][$key]["ProdPrecioVenta"] = number_format($precioFinal, 2);
+        }
+
+        $layout = "layout.view.tpl";
+
+        if(\Utilities\Security::isLogged())
+        {
+            $layout = "privatelayout.view.tpl";
+            \Utilities\Nav::setNavContext();
+        }
+
+        \Views\Renderer::render("index", $dataview, $layout);
     }
 }
 ?>
